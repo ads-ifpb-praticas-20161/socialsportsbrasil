54a5dc1dc6cd410f89b9f520153aa632e7513831
dijalmasilva <dijalmacz@gmail.com>
2016-06-19 22:04:12 -0300

    Adicionando o Spring framework - Spring data/ Spring boot

---
+++ ssb-web/src/main/java/dijalmasilva/ssb/web/app/AppConfig.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dijalmasilva.ssb.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author dijalma
+ */
+@SpringBootApplication
+public class AppConfig {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
---
+++ ssb-web/src/main/java/dijalmasilva/ssb/web/controladores/ControladorHome.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dijalmasilva.ssb.web.controladores;
+
+import java.io.IOException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author dijalma
+ */
+@Controller
+public class ControladorHome {
+    
+    @RequestMapping("/")
+    public String index() throws IOException{
+        return "/home";
+    }
+    
+    @RequestMapping("/home")
+    public String home(){
+        return "index";
+    }
+}
